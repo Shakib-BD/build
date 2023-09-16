9b919c02d80ad8a88b20c244b5884eee950f05fa
Mitsuaa <127077483+mitsu00@users.noreply.github.com>
2023-09-16 12:22:48 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/exTHmUI/android -b Tenshi -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsu00/local_manifest.git --depth 1 -b exthm-13 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b alpha-13 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # check
 echo ""
