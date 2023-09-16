41d0d60bc1639c0ac05bf83f28af555b4464552c
Mitsuaa <127077483+mitsu00@users.noreply.github.com>
2023-09-16 09:25:26 +0700

    Exthm

--- sync.sh
+++ sync.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsu00/local_manifest.git --depth 1 -b evox-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/exTHmUI/android -b Tenshi -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mitsu00/local_manifest.git --depth 1 -b exthm-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
+
+# check
+echo ""
+echo "Check home dir:"
+ls ../
+echo "Check work dir:"
+ls
+echo "Check complete"
