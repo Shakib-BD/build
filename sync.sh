a887f2b2b318eadc7ea11674490a78ae1782623a
Mitsuaa <127077483+mitsu00@users.noreply.github.com>
2023-09-17 12:22:35 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -11,13 +11,20 @@ echo "Check work dir:"
 ls
 echo "Check complete"
 
-# KernelSU Patch
+# kernel: KernelSU Patch
 wget https://raw.githubusercontent.com/mitsuav/workflow-test/main/KernelSU.patch
 cd kernel/xiaomi/mt6768
 git apply ../../../KernelSU.patch
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
 
-# Nikita GCam PATCH
+# mt6768-common: Nikita GCam PATCH
 cd ../../../
 rm device/xiaomi/mt6768-common/GCamPrebuilt/GCamPrebuilt.apk
 wget https://api.ozip.my.id/NGCam_8.2.300-v1.8.apk -O device/xiaomi/mt6768-common/GCamPrebuilt/GCamPrebuilt.apk
+
+# Media: mediatek: codecs & extractor PATCH
+wget https://api.ozip.my.id/media_codecs.patch
+wget https://api.ozip.my.id/media_extractor.patch
+cd frameworks/av
+git apply ../../media_codecs.patch
+git apply ../../media_extractor.patch
