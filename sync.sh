c99c211205810904e938dee901c111a51c5cd3b3
Mitsuaa <127077483+mitsu00@users.noreply.github.com>
2023-09-18 09:51:35 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-repo init --depth=1 --no-repo-verify -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r74 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsu00/local_manifest.git --depth 1 -b aosp .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu-jem -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mitsu00/local_manifest.git --depth 1 -b evox-13 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # check
 echo ""
@@ -19,8 +19,8 @@ curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh
 
 # mt6768-common: Nikita GCam PATCH
 cd ../../../
-#rm device/xiaomi/mt6768-common/GCamPrebuilt/GCamPrebuilt.apk
-#wget -q https://api.ozip.my.id/NGCam_8.2.300-v1.8.apk -O device/xiaomi/mt6768-common/GCamPrebuilt/GCamPrebuilt.apk
+rm device/xiaomi/mt6768-common/GCamPrebuilt/GCamPrebuilt.apk
+wget -q https://api.ozip.my.id/NGCam_8.2.300-v1.8.apk -O device/xiaomi/mt6768-common/GCamPrebuilt/GCamPrebuilt.apk
 
 # Media: mediatek: codecs & extractor PATCH
 wget -q https://api.ozip.my.id/media_codecs.patch
@@ -30,6 +30,6 @@ git am ../../media_codecs.patch
 git am ../../media_extractor.patch
 
 # Evox: Change default wallpaper (W.I.P)
-#cd ../../vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-nodpi
-#rm default_wallpaper.png
-#wget -q https://api.ozip.my.id/default_wallpaper.png
+cd ../../vendor/evolution/rro_overlays/EvolutionXConfigOverlay/res/drawable-nodpi
+rm default_wallpaper.png
+wget -q https://api.ozip.my.id/default_wallpaper.png
